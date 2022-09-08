cd3c0198f3a89b173174d8a2413f27406be00b21
OBogdana <dytynchukbogdana@gmail.com>
2022-09-08 15:50:34 +0300

    delete render.js,function rerenderPage moving to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import {rerenderPage} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import state from "./redux/state";
+import {addPost, updateNewPostText} from "./redux/state";
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderPage = () => {
+
+    root.render(
+        <React.StrictMode>
+            <App state={state}
+                 addPost={addPost}
+                 updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>
+    );
+}
 rerenderPage(state);
 
 
