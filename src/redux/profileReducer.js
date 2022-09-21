e506b955649c22e443d4ddbf6f625615c823480f
OBogdana <dytynchukbogdana@gmail.com>
2022-09-21 17:48:44 +0300

    install redux

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,7 +1,27 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState =  {
+    posts: [
+        {
+            id: 1,
+            ava: "https://cdn.meta.ua/meta_news/fa/01000ydd-fa29_1280x720.jpeg",
+            message: "AAAAAAAAAAAAAAAAAA-post",
+            likeCount: 9,
+        },
+
+        {
+            id: 2,
+            ava: "https://knowhow.pp.ua/wp-content/uploads/2020/05/unnamed-2.jpg",
+            message: "BBBBBBBBBBBBBBBBBB-post",
+            likeCount: 3,
+        },
+    ],
+    newPostBody: 'What do you say?',
+};
+
+const profileReducer = (state = initialState, action) => {
+
     switch (action.type) {
         case ADD_POST:
             let newPost = {
