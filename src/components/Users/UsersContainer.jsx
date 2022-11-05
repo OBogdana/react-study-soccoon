3c5872058d7e686c610d410908f574beb80550d8
OBogdana <dytynchukbogdana@gmail.com>
2022-11-05 12:18:29 +0200

    UsersContainer and presentation components

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-import Users from "./UsersConteinerAPI";
+import Users from "./Users";
 import {
     followActionCreator,
     setActivePageActionCreator,
@@ -8,6 +8,36 @@ import {
     unfollowActionCreator,
     setTotalUsersCountActionCreator,
 } from "../../redux/usersReducer";
+import axios from "axios";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`http://localhost:3001/users?_page=1&_limit=2`)
+            .then(response => {
+                this.props.setUsers(response.data);
+                this.props.setTotalUsersCount(response.data.totalUsersCount);
+            });
+    }
+
+    onPageChanged = (page) => {
+        this.props.setActivePage(page);
+        axios.get(`http://localhost:3001/users?_page=${page}&_limit=2`)
+            .then(response => {
+                this.props.setUsers(response.data);
+            });
+    }
+
+    render() {
+        return <Users totalUsersCount = {this.props.totalUsersCount}
+                      limit = {this.props.limit}
+                      page = {this.props.page}
+                      onPageChanged = {this.onPageChanged}
+                      users = {this.props.users}
+                      unfollow = {this.props.unfollow}
+                      follow = {this.props.follow}
+        />
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -39,5 +69,5 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
